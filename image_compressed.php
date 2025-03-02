33d1ee26478ea5d0384c6be4b5d50ec6cc550802
Maartje Eyskens <maartje@eyskens.me>
2025-03-02 19:23:22 +0100

    support png images and do not call headers first

--- image_compressed.php
+++ image_compressed.php
@@ -3,6 +3,11 @@ $url = "";
 $filetype = "";
 $raw_image = NULL;
 
+$supports_png = false;
+if (strpos($_SERVER['HTTP_ACCEPT'], 'image/png') !== false) {
+    $supports_png = true;
+}
+
 //get the image url
 if (isset( $_GET['i'] ) ) {
     $url = $_GET[ 'i' ];
@@ -17,48 +22,38 @@ if (substr( $url, 0, 4 ) != "http") {
     exit();
 }
 
-$context = stream_context_create(['http' => array('method' => 'HEAD')]);
-$headers = get_headers($url, true, $context);
-
-if (array_key_exists('Content-Type', $headers)) {
-    $headers['content-type'] = $headers['Content-Type'];
-}
-
-if (!array_key_exists('content-type', $headers)) {
-    echo "Failed to get the image, its server did not return expected details :(";
-    exit();
-}
-
-$allowed_types = ["image/jpeg", "image/png"];
-
-if (!in_array($headers['content-type'], $allowed_types)) {
-    echo("Unsupported file type :( " . $headers['content-type']);
-    exit();
-}
-
 // get the image
 $raw_image = file_get_contents($url);
 
 $im1 = new Imagick();
-$im1->readImageBlob($raw_image);
+try {
+    $im1->readImageBlob($raw_image);
+} catch (ImagickException $e) {
+    echo("Failed to read image :(");
+    exit();
+}
 
 // Resize the image to 100 pixels in width (height auto-adjusts)
 $im1->resizeImage(100, 0, Imagick::FILTER_LANCZOS, 1);
 
-// get image width and height
-$width = $im1->getImageWidth();
+if (!$supports_png) {
+    // get image width and height
+    $width = $im1->getImageWidth();
 
-// Apply Floyd-Steinberg dithering and remap to a grayscale pattern
-$palette = new Imagick();
-$palette->newPseudoImage(100, $width, "pattern:gray50");
-$im1->remapImage($palette, Imagick::DITHERMETHOD_FLOYDSTEINBERG);
+    // Apply Floyd-Steinberg dithering and remap to a grayscale pattern
+    $palette = new Imagick();
+    $palette->newPseudoImage(100, $width, "pattern:gray50");
+    $im1->remapImage($palette, Imagick::DITHERMETHOD_FLOYDSTEINBERG);
 
-// Output the image as a BMP format and store it in a variable
-$im1->setImageFormat('wbmp');
+    // Output the image as a BMP format and store it in a variable
+    $im1->setImageFormat('wbmp');
+    header('Content-Type: image/vnd.wap.wbmp');
+} else {
+    $im1->setImageFormat('png');
+    header('Content-Type: image/png');
+}
 $bmpData = $im1->getImageBlob();
 
-// Optionally, display or save the output
-header('Content-Type: image/vnd.wap.wbmp');
 echo $bmpData;
 
 // Cleanup
